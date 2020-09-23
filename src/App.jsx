c1d513097cb74dcfa28e37dd089f02a1bd18351b
Dustin Tollett <dustintollett@gmail.com>
2020-09-23 10:23:30 -0400

    finished homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,74 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  state = {
+    hue: 75,
+    saturation: 75,
+    lightness: 75,
+  }
+
+  handleMoveHueSlider = event => {
+    this.setState({ hue: event.target.value })
+    console.log(event.target.value)
+  }
+  handleMoveSaturationSlider = event => {
+    this.setState({ saturation: event.target.value })
+    console.log(event.target.value)
+  }
+  handleMoveLightnessSlider = event => {
+    this.setState({ lightness: event.target.value })
+    console.log(event.target.value)
+  }
   render() {
-    return <div>Hello, World!</div>
+    const currentColor = `hsl(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%)`
+
+    return (
+      <>
+        <header>
+          <h1>COLOR PICKER</h1>
+        </header>
+        <body>
+          <figure style={{ backgroundColor: currentColor }}></figure>
+          <div>
+            <section>
+              <h2>HUE {this.state.hue}</h2>
+              <input
+                type="range"
+                name="Hue"
+                min="0"
+                max="360"
+                onChange={this.handleMoveHueSlider}
+              ></input>
+            </section>
+            <section>
+              <h2>SATURATION {this.state.saturation}%</h2>
+              <input
+                type="range"
+                id="saturation"
+                name="saturation"
+                min="0"
+                max="100"
+                onChange={this.handleMoveSaturationSlider}
+              ></input>
+            </section>
+            <section>
+              <h2>LIGHTNESS {this.state.lightness}%</h2>
+              <input
+                type="range"
+                id="lightness"
+                name="lightness"
+                min="0"
+                max="100"
+                onChange={this.handleMoveLightnessSlider}
+              ></input>
+            </section>
+          </div>
+          <div>
+            <button>Random Color</button>
+          </div>
+        </body>
+      </>
+    )
   }
 }
 
